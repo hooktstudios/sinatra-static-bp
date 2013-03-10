1f1245f4f957bf4c11325edf521b9b8a7e4568c2
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-03-10 13:17:46 -0400

    Improve first deploy logic for public folder
    ./public doesn't exist at first

--- lib/skeleton/config/deploy.rb
+++ lib/skeleton/config/deploy.rb
@@ -6,7 +6,8 @@ set :default_stage, 'development'
 set :s3, YAML::load( File.open( File.expand_path( '../s3.yml', __FILE__ ) ) )
 
 before 'deploy' do
-  run_locally "rm -rf public/*"
+  run_locally "rm -rf ./public"
+  run_locally "mkdir public"
   run_locally "export RACK_ENV=production && bundle exec rake sinatra:export"
   run_locally "export RACK_ENV=production && bundle exec rake assetpack:build"
 end
